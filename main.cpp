ee2dcf347698bafb7558791521f4cc5def2da7d7
denisa2904 <mariadenisa2904@gmail.com>
2021-12-09 13:16:42 +0200

    Revert "gatu"
    This reverts commit 566b3aa5f595b059b0bb21820004b1b0463bd2f9.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <graphics.h>
 #include <winbgim.h>
 #include <cmath>
-#define bulangiu 1
+
 using namespace std;
 
 
@@ -142,7 +142,7 @@ void initMeniu()
     settextstyle(6, HORIZ_DIR, 1);
     outtextxy(207, 120,"MULTIPLAYER");
     int x,y;
-    int gatu=bulangiu;
+    int gatu=1;
     while(gatu>0){
         if(ismouseclick(WM_LBUTTONDOWN))
         {
